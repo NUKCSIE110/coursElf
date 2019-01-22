7925b48d53236147890c0433aaa94c6abceffa53
ifTNT <happyjohn369@gmail.com>
2019-01-22 15:27:44 +0800

    Completed basic layout

---
+++ public/js/layout.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  $(".titleBar .burger").click(() => {
+    $(".titleBar nav").slideToggle();
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', loggedin: false});
 });
 
 module.exports = router;
