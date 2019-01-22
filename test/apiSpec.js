ac266df421c68d3d72d711be443c1f71a548b9aa
ifTNT <happyjohn369@gmail.com>
2019-01-22 10:08:35 +0800

    more BDD

--- test/apiSpec.js
+++ test/apiSpec.js
@@ -2,21 +2,21 @@ const request = require("supertest");
 const app = require("../app");
 require("should");
 
-describe("GET /api/test", function() {
-  it("respond with json", function(done) {
+describe("GET /api/test", () => {
+  let result = {};
+  it("respond with json", () => {
     request(app)
       .get("/api/test")
       .set("Accept", "application/json")
       .end(function(err, res) {
-        if (err) return done(err); //server error
-        if (res.statusCode !== 200) return done(res.body); //api error;
-
-        const result = res.body;
-        result.should.be.Object();
-        result.should.have.keys("msg");
-        result.msg.should.be.equal("Succeed");
-
-        done();
+        if (err) return err; //server error
+        if (res.statusCode !== 200) return res.body; //api error;
+        result = res.body;
       });
+  });
+  it("Response should contain msg field", () => {
+    result.should.be.Object();
+    result.should.have.keys("msg");
+    result.msg.should.be.equal("Succeed");
   });
 });
