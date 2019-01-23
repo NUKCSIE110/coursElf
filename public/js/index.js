b2f34bea0355eebec39646296236a506591601db
ifTNT <happyjohn369@gmail.com>
2019-01-23 18:41:01 +0800

    Fix query align-items

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
-const Homepage=true
-$(".titleBar .allCourses").click(function(){
-    $(this).addClass('selected');
-    $('ul.timeline').hide();
-    $('.query').show();
-    window.history.pushState({},'','/query');
+const Homepage = true;
+$(".titleBar .allCourses").click(function() {
+  $(this).addClass("selected");
+  $(".content").css("align-items", "flex-start");
+  $("ul.timeline").hide();
+  $(".query").show();
+  window.history.pushState({}, "", "/query");
 });
