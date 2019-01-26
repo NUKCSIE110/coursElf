e08d9a2889d732553bf28f2ea545e867cf3db95a
ifTNT <happyjohn369@gmail.com>
2019-01-26 19:28:52 +0800

    Helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var stylus = require("stylus");
+var helmet = require('helmet');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -16,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
