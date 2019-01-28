354c3f586b8db6504552d862d61256d90777e140
ifTNT <happyjohn369@gmail.com>
2019-01-28 08:13:55 +0800

    Display done course

--- routes/users.js
+++ routes/users.js
@@ -63,4 +63,36 @@ router.get("/logout", function(req, res, next) {
   res.redirect("/");
 });
 
+router.get("/mycourse", function(req, res, next) {
+  if (req.session.loggedin !== true) {
+    res.redirect("/");
+  } else {
+    let calcPoint = (acc, e) => (acc += parseInt(e[2]));
+    let passed = req.session.doneCourse.filter(x => x[3] >= 60);
+    let common = passed.filter(x => x[0].match(/^GR.+/));
+    let chinese = common
+      .filter(x => x[1].match(/^中文.+/))
+      .reduce(calcPoint, 0);
+    let english = common
+      .filter(x => x[1].match(/^英語會話與閱讀.+/))
+      .reduce(calcPoint, 0);
+    common = { chinese, english };
+
+    let cc = passed.filter(x => x[0].match(/^CC.+/)).reduce(calcPoint, 0);
+
+    let li = passed.filter(x => x[0].match(/^LI.+/)).reduce(calcPoint, 0);
+    let sc = passed.filter(x => x[0].match(/^SC.+/)).reduce(calcPoint, 0);
+    let so = passed.filter(x => x[0].match(/^SO.+/)).reduce(calcPoint, 0);
+    let by = { li, sc, so };
+
+    let rest = passed
+      .filter(x => x[0].match(/^(?!GR|CC|LI|SC|SO)/))
+      .reduce(calcPoint, 0);
+
+    let donePoint = { common, cc, by, rest };
+    console.log(donePoint);
+    res.render("mycourse", { loggedin: req.session.loggedin ,doneCourses: passed, donePoint });
+  }
+});
+
 module.exports = router;
