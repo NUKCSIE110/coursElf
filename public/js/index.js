62ea17e86b610be00e16fed2617bd21468829e82
ifTNT <happyjohn369@gmail.com>
2019-01-31 20:56:29 +0800

    Fix date issue

--- public/js/index.js
+++ public/js/index.js
@@ -13,12 +13,12 @@ function init() {
   let draw = SVG("timeline").size(width, height);
 
   let lables = [
-    { text: "第一階段開始", time: new Date("2019-01-28 09:00:00 UTC+8") },
-    { text: "第一階段結束", time: new Date("2019-02-01 12:00:00 UTC+8") },
-    { text: "第二階段開始", time: new Date("2019-02-11 12:00:00 UTC+8") },
-    { text: "第二階段結束", time: new Date("2019-02-15 12:00:00 UTC+8") },
-    { text: "加退選開始", time: new Date("2019-02-18 09:00:00 UTC+8") },
-    { text: "加退選結束", time: new Date("2019-02-28 23:59:59 UTC+8") }
+    { text: "第一階段開始", time: new Date(2019, 0, 28, 9, 0, 0) },
+    { text: "第一階段結束", time: new Date(2019, 1, 1, 12, 0, 0) },
+    { text: "第二階段開始", time: new Date(2019, 1, 11, 12, 0, 0) },
+    { text: "第二階段結束", time: new Date(2019, 1, 15, 12, 0, 0) },
+    { text: "加退選開始", time: new Date(2019, 1, 18, 9, 0, 0) },
+    { text: "加退選結束", time: new Date(2019, 1, 28, 23, 59, 59) }
   ];
   let lineStart = 5;
   let lineEnd = height - 5;
@@ -54,7 +54,7 @@ function init() {
       .fill("#B0C4DE")
       .center(centerLine, threshold)
       .animate(1500, "<>")
-      .attr({fill:"#4682B4"})
+      .attr({ fill: "#4682B4" })
       .loop(true, true);
   }
 
