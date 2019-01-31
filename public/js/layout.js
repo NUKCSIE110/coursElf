9ab5f43a41f59e851e3f66139e3c8188adc59c1e
ifTNT <happyjohn369@gmail.com>
2019-01-31 20:17:23 +0800

    Progressive web app

--- public/js/layout.js
+++ public/js/layout.js
@@ -20,6 +20,17 @@ function init() {
   $(window).resize(function() {
     window.scrollTo(0, 1);
   });
+
+  //Register service-worker
+  if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(reg){
+        console.log("Yes, it did.");
+      }).catch(function(err) {
+        console.log("No it didn't. This happened: ", err)
+      });
+  }
   // window.addEventListener("beforeinstallprompt", e => {
   //   // Prevent Chrome 67 and earlier from automatically showing the prompt
   //   e.preventDefault();
