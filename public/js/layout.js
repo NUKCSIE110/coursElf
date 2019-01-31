dd9eacfb78658f563358cbbbb6f778c87d499cc8
ifTNT <happyjohn369@gmail.com>
2019-01-31 19:58:02 +0800

    Added manifest.json

--- public/js/layout.js
+++ public/js/layout.js
@@ -20,5 +20,11 @@ function init() {
   $(window).resize(function() {
     window.scrollTo(0, 1);
   });
+  // window.addEventListener("beforeinstallprompt", e => {
+  //   // Prevent Chrome 67 and earlier from automatically showing the prompt
+  //   e.preventDefault();
+  //   // Stash the event so it can be triggered later.
+  //   alert("oh ya");
+  // });
 }
 init();
